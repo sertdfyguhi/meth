9886a493fe350a5e265a5aa6820df89f737c9c25
sertdfyguhi-git <gdkamtalan@gmail.com>
2023-08-01 01:16:23 +0800

    v1.2.0

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
+import timeit
 import meth
 
-print(meth.parse("(1 + 2)(2x(2))"))
+expr = meth.tokenize("1 + (2(2 * x) ^ 2) % 5")
+
+# old_p = meth.Parser()
+new_p = meth.Parser()
+
+
+def new():
+    new_p.parse(expr)
+
+
+print("new:", timeit.timeit(new, number=10000, globals=globals()))
+# print("old:", timeit.timeit(old, number=10000, globals=globals()))
