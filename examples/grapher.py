36111ca15327226de73e13231eaff24c34e60842
sertdfyguhi-git <gdkamtalan@gmail.com>
2026-02-09 22:20:07 +0800

    update grapher

--- examples/grapher.py
+++ examples/grapher.py
@@ -3,30 +3,46 @@ import numpy as np
 import meth
 
 STEP = 1 / 10
-
+X_LIMIT = (-10, 10)
+Y_LIMIT = (-10, 10)
 
 print("Simple equation grapher made using meth.")
 
-equation = input("Input equation to graph (use x, y): ")
-ast = meth.parse(meth.tokenize(equation))
+x_or_y = input("Axis (x or y, default: y): ").strip().lower() or "y"
+if x_or_y not in "xy":
+    print("Axis has to be x or y.")
+    exit(0)
 
-start = int(input("Input start (default: -10): ") or -10)
-end = int(input("Input end (default: 10): ") or 10)
+equation = input(f"Input equation to graph (use x, y)\n{x_or_y} = ")
+ast = meth.parse(equation)
 
-x_points = np.arange(start, end, STEP)
-y_points = []
+start = int(input("Input start (default: -20): ") or -20)
+end = int(input("Input end (default: 20): ") or 20)
 
-if isinstance(ast, meth.AssignNode):
-    pass
+a_points = np.arange(start, end, STEP)
+b_points = []
+
+evaluator = meth.Evaluator()
+
+for a in a_points:
+    evaluator.set_var("x" if x_or_y == "y" else "y", a)
+    b = evaluator.evaluate(ast)
+    b_points.append(b)
+
+full_equation = f"{x_or_y} = {equation}"
+
+if x_or_y == "y":
+    plt.plot(a_points, b_points, label=full_equation)
 else:
-    # y = x
-    evaluator = meth.Evaluator()
+    plt.plot(b_points, a_points, label=full_equation)
 
-    for x in x_points:
-        evaluator.set_var("x", x)
-        y = evaluator.evaluate(ast)
-        y_points.append(y)
-
-plt.plot(x_points, y_points)
-plt.grid(True)
+plt.title(f"Graph of {full_equation}")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.axhline(0, color="black", linewidth=1)
+plt.axvline(0, color="black", linewidth=1)
+plt.grid(True, linestyle="--", alpha=0.5)
+plt.legend()
+plt.xlim(X_LIMIT)
+plt.ylim(Y_LIMIT)
 plt.show()
